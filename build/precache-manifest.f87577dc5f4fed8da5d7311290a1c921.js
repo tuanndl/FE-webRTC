7569e75289dac211966a362926261f922ea8c684
nguyen duc linh tuan <tuan2887903@gmail.com>
2021-02-23 20:37:32 +0700

    build

---
+++ build/precache-manifest.f87577dc5f4fed8da5d7311290a1c921.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "244d59f3ff0444a64126327de7bdf8da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6457ae040ffbf5cbcaa5",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ddc1188d5fb153ccbe6e",
+    "url": "/static/js/2.cf29a11c.chunk.js"
+  },
+  {
+    "revision": "b67bc97fd0dd67004287af4028afc1b1",
+    "url": "/static/js/2.cf29a11c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6457ae040ffbf5cbcaa5",
+    "url": "/static/js/main.100e028e.chunk.js"
+  },
+  {
+    "revision": "702554416a81a1e3a894",
+    "url": "/static/js/runtime-main.784c586e.js"
+  }
+]);
